e42843c3c37231b716c8912ad14dd33167a7fa4d
Zlatin Stanimirov <4281340+stanimirovv@users.noreply.github.com>
2019-12-22 16:48:40 +0200

    Add files via upload

---
+++ bag_of_words.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+
+def load_and_parse_data():
+    chalga_lyrics = pd.read_json('a.json')
+    rap_lyrics = pd.read_json('b.json')
+
+    chalga_lyrics["type"] = 0
+    rap_lyrics["type"] = 1
+
+
+    data = pd.concat([chalga_lyrics, rap_lyrics])
+    data = data.sample(frac=1)
+
+    print(data.head())
+    return data
+
+def train_and_test_model(data):
+    vectorizer = CountVectorizer()
+    data_bow = vectorizer.fit_transform(data["lyrics"])
+    
+    print(vectorizer.get_feature_names())
+    return
+
+    KK = [10, 100, 200, 300, 350]
+
+    for K in KK:
+        data_bow_train = data_bow[:K]
+        data_target_train = data[:K]
+        data_bow_test = data_bow[K:]
+        data_target_test = data[K:]
+
+        # Logistic
+        print('Logistic regression')
+        clf = LogisticRegression(random_state=0).fit(data_bow_train, data_target_train["type"])
+        print(clf.score(data_bow_test, data_target_test["type"]))
+
+        #SVM
+        print('Support Vector Machines')
+        clf = svm.SVC()
+        clf.fit(data_bow_train, data_target_train["type"])
+        print(clf.score(data_bow_test, data_target_test["type"]))
+
+data = load_and_parse_data()
+train_and_test_model(data)
+
